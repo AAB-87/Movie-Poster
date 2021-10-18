db5b0541e3aa02c7fdd880ae0857cd4c317f2b68
AAB-87 <andrej-abrekov@yandex.ru>
2021-10-18 16:05:00 +0400

    Added tests

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Test;
 import ru.netology.domain.MoviePoster;
 import ru.netology.manager.MovieManager;
 import org.junit.jupiter.api.BeforeEach;
@@ -5,40 +6,57 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MovieManagerTest {
 
+class ManagerTest { // Подготовка начального состояния
+    public MovieManager manager = new MovieManager();
 
-    public class ManagerTest {
+    private MoviePoster first = new MoviePoster(1, 151, "Бладшот");
+    private MoviePoster second = new MoviePoster(2, 152, "Вперёд");
+    private MoviePoster third = new MoviePoster(3, 153, "Отель Белград");
+    private MoviePoster fourth = new MoviePoster(4, 154, "Джентельмены");
+    private MoviePoster fifth = new MoviePoster(5, 155, "Человек-Невидимка");
+    private MoviePoster sixth = new MoviePoster(6, 156, "Тролли");
+    private MoviePoster seventh = new MoviePoster(7, 157, "Номер один");
+    private MoviePoster eighth = new MoviePoster(8, 158, "Такси");
+    private MoviePoster ninth = new MoviePoster(9, 159, "Судный день");
+    private MoviePoster tenth = new MoviePoster(10, 160, "Неудержимые");
+    private MoviePoster eleventh = new MoviePoster(11, 161, "Отступники");
 
-        public MovieManager manager = new MovieManager();
+    @BeforeEach
 
-        private Movie first = new Movie(1, 151, "Бладшот");
-        private Movie second = new Movie(2, 152, "Вперёд");
-        private Movie third = new Movie(3, 153, "Отель Белград");
-        private Movie fourth = new Movie(4, 154, "Джентельмены");
-        private Movie fifth = new Movie(5, 155, "Человек-Невидимка");
-        private Movie sixth = new Movie(6, 156, "Тролли");
-        private Movie seventh = new Movie(7, 157, "Номер один");
-        private Movie eighth = new Movie(8, 158, "Такси");
-        private Movie ninth = new Movie(9, 159, "Судный день");
-        private Movie tenth = new Movie(10, 160, "Неудержимые");
-
-
-        @BeforeEach
-
-        public void setUp() {
-            manager.save(first);
-            manager.save(second);
-            manager.save(third);
-            manager.save(fourth);
-            manager.save(fifth);
-            manager.save(sixth);
-            manager.save(seventh);
-            manager.save(eighth);
-            manager.save(ninth);
-            manager.save(tenth);
-
-        }
+    public void setUp() {
+        manager.save(first);
+        manager.save(second);
+        manager.save(third);
+        manager.save(fourth);
+        manager.save(fifth);
+        manager.save(sixth);
+        manager.save(seventh);
+        manager.save(eighth);
+        manager.save(ninth);
+        manager.save(tenth);
+        manager.save(eleventh);
     }
 
+    @Test
+    void shouldSaveMovies() {
+        MovieManager manager = new MovieManager();
+        manager.save(first);
+        MoviePoster[] expected = new MoviePoster[]{first};
+        MoviePoster[] actual = manager.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void getLast10Movies() {
+        MovieManager manager = new MovieManager();
+
+        MoviePoster[] expected = new MoviePoster[]{eleventh, tenth, ninth, eighth, seventh, sixth, fifth, fourth, third, second};
+        MoviePoster[] actual = manager.getAll();
+
+        assertArrayEquals(expected, actual);
+
+    }
 }
+
+
